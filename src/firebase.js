5951b5cd73c55ac69a164f72f1852cc8271fa9c2
Anuj Ambadas Giri <87684592+anujgiri20@users.noreply.github.com>
2022-06-29 23:16:09 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,6 @@ import 'firebase/database';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDu4Z7b7LTGLLl8pQoWmQRHvMFFD6P1WGM",
-  authDomain: "newone-675a0.firebaseapp.com",
-  projectId: "newone-675a0",
-  storageBucket: "newone-675a0.appspot.com",
-  messagingSenderId: "1021355394323",
-  appId: "1:1021355394323:web:688e66dd94af4829f6c6e6",
-  measurementId: "G-8CN5069H55"
 };
 
 
